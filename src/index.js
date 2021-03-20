1cd576487885e5ba5687b4b7a53a46728d1c8754
Kelwin Ladeira <kelwin.ladeira@smarthis.com.br>
2021-03-20 20:54:02 -0300

    Challenge Completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,74 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    response.status(404).json({ error: "User does not exists!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
-function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
-}
+function checksCreateTodosUserAvailability(request, response, next) { 
+  const { user } = request;
+
+  if(user.pro === true){
+    return next();
+  }
+
+  if (user.todos.length >= 10) {
+    return response
+      .status(403)
+      .json({
+        error:
+          "Activate the pro plan or decrease the total amount to allocate this new",
+      });
+  }
+
+  return next();
+} 
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User does not exist!" });
+  }
+
+  const isIdValid = validate(id);
+  if (!isIdValid) {
+    return response.status(400).json({ error: "Id does not valid!" });
+  }
+  const todo = user.todos.find((toDo) => toDo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Todo does not exist!" });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({error: "User does not exist!"})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
